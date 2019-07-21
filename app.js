81dfc934f25987e26827f3e01d70a42db7641890
Ali Ezadkhaha <hodhod212@yahoo.com>
2019-07-21 11:09:59 +0200

    Global app controller

---
+++ app.js
@@ -0,0 +1,30 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
+    
+   
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+
+})();
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+    document.querySelector('.add__btn').addEventListener('click',function(){
+        // 1. Get the field input data
+      
+        // 2. Add the item to the budget controller
+    
+
+        // 3. Add the item to the UI
+        
+
+        // 4. Clear the fields
+    
+
+        // 5. Calculate and update budget
+             
+             
+        // 6. Calculate and update percentages
+    });
+})();
