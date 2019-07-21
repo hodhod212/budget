5e0d4420924b62c7d14e2f924e5af24449137325
Ali Ezadkhaha <hodhod212@yahoo.com>
2019-07-21 15:04:42 +0200

    put all eventlistener functions inside a function

--- app.js
+++ app.js
@@ -6,25 +6,55 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-
+    var DOMstrings = {
+        inputType:'.add__type',
+        inputDescription:'.add__description',
+        inputValue:'.add__value',
+        inputBtn:'.add__btn'
+    }
+    return {
+        getInput: function(){
+            return {
+                typ: document.querySelector(DOMstrings.inputType).value,//will be either inc or exp.
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value 
+            };
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
+            
+        }
+    }
 })();
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    document.querySelector('.add__btn').addEventListener('click',function(){
-        // 1. Get the field input data
-      
-        // 2. Add the item to the budget controller
-    
-
-        // 3. Add the item to the UI
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+        document.addEventListener('keypress', function(event){
+        if (event.keyCode === 13 || event.which === 13) {
+           ctrlAddItem();
+        } 
         
-
-        // 4. Clear the fields
-    
-
-        // 5. Calculate and update budget
-             
-             
-        // 6. Calculate and update percentages
     });
-})();
+    }
+    
+    var ctrlAddItem = function(){
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI
+        // 4. Clear the fields
+        // 5. Calculate and update budget    
+        // 6. Calculate and update percentages
+        
+    };
+    return {
+        init: function(){
+            console.log('just a test');
+            setupEventListeners();
+        }
+    };
+})(budgetController,UIController);
+controller.init();
